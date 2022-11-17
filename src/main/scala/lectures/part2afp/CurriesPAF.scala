a8b3e3ebddea94c3ef5cc7a12e44f7384d2db09c
Filip Szydłak <266912@student.pwr.edu.pl>
2022-11-18 00:25:19 +0100

    CurriesPAF

---
+++ src/main/scala/lectures/part2afp/CurriesPAF.scala
@@ -0,0 +1,110 @@
+package lectures.part2afp
+
+object CurriesPAF extends App {
+
+  // curried functions
+  val superAdder: Int => Int => Int =
+    x => y => x + y
+
+  val add3 = superAdder(3) // Int => Int = y => 3 + y
+  println(add3(5))
+  println(superAdder(3)(5))
+
+  // METHOD! => you have to pass all parameter list
+  def curriedAdder(x: Int)(y: Int): Int = x + y // curried method
+
+  // convertion into a function of type Int => Int
+  val add4: Int => Int = curriedAdder(4)
+
+  // lifting => transforming a method to function => ETA-EXPANSION
+
+  // functions != methods (JVM limitation)
+  def inc(x: Int): Int = x + 1
+
+  List(1, 2, 3).map(x => inc(x)) // ETA-expansion
+
+  // Partial function applications
+  val add5 = curriedAdder(5) _ // Int => Int
+
+  // Exercise
+  val simpleAddFunction = (x: Int, y: Int) => x + y
+
+  def simpleAddMethod(x: Int, y: Int): Int = x + y
+
+  def curriedAddMethod(x: Int)(y: Int): Int = x + y
+
+  val add7 = x => simpleAddFunction(7, x)
+  val add7_2 = simpleAddFunction.curried(7)
+  val add7_6 = simpleAddFunction(7, _: Int) // works as well
+  val add7_3 = curriedAddMethod(7) _ // partially applied function
+  val add7_4 = curriedAddMethod(7)(_) // PAF - alternative syntax
+
+  val add7_5 = simpleAddMethod(7, _: Int) // alternative syntax for turning methods into function values
+  // y => simpleAddMethod(7,y)
+
+
+  // underscores are powerful
+  def concatenator(a: String, b: String, c: String) = a + b + c
+
+  val insertName = concatenator("Hello, I'm ", _: String, ", how are you") // x: String => concatenator(hello, x, howareyou)
+  println(insertName("Daniel"))
+
+  val fillIntheBlanks = concatenator("Hello", _: String, _: String) // (x, y) => concatenator ("Hello", ", x, y)
+  println(fillIntheBlanks("Daniel ", " Scala is awesome!"))
+
+  // EXERCISES
+  /*
+    1. Process a list of numbers and return their string representations with different formats
+      Use the %4.2f, %8.6f and %14.12f with a curried formatter function
+  */
+
+  def curriedFormatter(s: String)(number: Double): String = s.format(number)
+
+  val number = List(Math.PI, Math.E, 1, 9.8, 1.3 - 12)
+
+  val simpleFormat = curriedFormatter("%4.2f") _ // lift
+  val seriousFormat = curriedFormatter("%8.6f") _
+  val preciseFormat = curriedFormatter("%14.12f") _
+
+  println(number.map(simpleFormat))
+  println(number.map(curriedFormatter("%14.12f"))) // compiler does sweet eta- expansion for us
+
+  /*
+    2. difference between
+      - functions vs methods
+      - parameters: by-name vs 0-lambda
+  */
+
+  def byName(n: => Int) = n + 1
+
+  def byFunction(f: () => Int) = f() + 1
+
+  def method: Int = 42
+
+  def parenMethod(): Int = 42
+
+  /*
+    calling byName and byFunction
+    - int
+    - method
+    - lambda
+    - PAF
+  */
+
+  byName(23) // ok
+  byName(method) // ok
+  byName(parenMethod())
+  // byName(parenMethod) Scala 3 does not compile parenMethod without ()
+  // BY NAME USES ONLY A VALUE OF THE METHOD,  NOT THE METHOD ITSELF
+  // byName(() => 42) not ok
+  byName((() => 42) ())
+  // byName(parenMethod _) not ok
+
+
+
+  //byFunction(45) not ok
+  //byFunction(method) not ok!!!!!!!! deos not do ETA-expansion
+  byFunction(parenMethod)// compiler does ETA - expansion
+  byFunction(() => 46) // works
+  byFunction(parenMethod _) // also works, but warning - unnecessary
+}
